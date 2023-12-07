b0f3edefd6100217a91717d7a7222a59bfd6bb19
manojastikar <manojastikar80@gmail.com>
2023-12-08 02:50:14 +0530

    First Commit of basic socket.io project

---
+++ basic_realtime_project/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require('cors');
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
+});
+
+io.on("connection", (socket) => {
+    console.log(`a user is connected ${socket.id}`);
+
+    socket.on("send_message", (data) => {
+        data.message = "Namaste " + data.message;
+        socket.broadcast.emit("receive_message", data);
+    });
+});
+
+server.listen(4000, () => { console.log("listening by Manoj's server on port 4000") });
